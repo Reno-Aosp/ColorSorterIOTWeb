c7ec8a752740d1ddf2928d4abf6280018f4bab26
Ren-Aosp <marino1putra@gmail.com>
2025-09-08 23:36:54 +0700

    add more colors support

--- src/main/resources/static/FrontEnd/sourcecode/components/PieChart.jsx
+++ src/main/resources/static/FrontEnd/sourcecode/components/PieChart.jsx
@@ -4,6 +4,7 @@ import * as echarts from 'echarts';
 // Add the missing getColorHex function
 const getColorHex = (colorName) => {
   const colorMap = {
+    // Basic colors
     red: '#FF0000',
     blue: '#0000FF', 
     green: '#00FF00',
@@ -15,7 +16,57 @@ const getColorHex = (colorName) => {
     black: '#000000',
     white: '#FFFFFF',
     gray: '#808080',
-    grey: '#808080'
+    grey: '#808080',
+    
+    // Extended colors
+    cyan: '#00FFFF',
+    magenta: '#FF00FF',
+    lime: '#32CD32',
+    navy: '#000080',
+    maroon: '#800000',
+    olive: '#808000',
+    teal: '#008080',
+    silver: '#C0C0C0',
+    gold: '#FFD700',
+    
+    // Fancy colors
+    turquoise: '#40E0D0',
+    coral: '#FF7F50',
+    salmon: '#FA8072',
+    khaki: '#F0E68C',
+    plum: '#DDA0DD',
+    orchid: '#DA70D6',
+    crimson: '#DC143C',
+    indigo: '#4B0082',
+    violet: '#EE82EE',
+    azure: '#F0FFFF',
+    
+    // Shades
+    lightblue: '#ADD8E6',
+    darkblue: '#00008B',
+    lightgreen: '#90EE90',
+    darkgreen: '#006400',
+    lightred: '#FFB6C1',
+    darkred: '#8B0000',
+    lightyellow: '#FFFFE0',
+    darkyellow: '#B8860B',
+    
+    // Fun colors
+    hotpink: '#FF69B4',
+    deeppink: '#FF1493',
+    skyblue: '#87CEEB',
+    forestgreen: '#228B22',
+    royalblue: '#4169E1',
+    firebrick: '#B22222',
+    chocolate: '#D2691E',
+    sandybrown: '#F4A460',
+    
+    // Neon colors
+    neongreen: '#39FF14',
+    neonblue: '#1B03A3',
+    neonpink: '#FF073A',
+    neonyellow: '#CCFF00',
+    neonorange: '#FF6600'
   };
   
   return colorMap[colorName?.toLowerCase()] || '#CCCCCC';
